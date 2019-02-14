4fd6573a32f95871ec5bd17b7e3c76dea9e81a8d
Christoffer Berg <cgbjensen@gmail.com>
2019-02-14 22:40:30 +0100

    reformatted code

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 /*
   Import main.scss as the Parcel bundler needs an entry file
 */
-import './main.scss';
+import "./main.scss";
 
-console.log('hello from main.ts');
+console.log("hello from main.ts");
